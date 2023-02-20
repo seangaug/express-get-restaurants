bfcfdbb840c8bfff21b4a5f9bdc7e0fbfee3a995
seangaug <seanjnr1991@aol.com>
2023-02-20 15:29:11 +0000

    Express GET request in server.js

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const {sequelize} = require("./db");
 const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
-
+app.get("/restaurants", async (req, res) => {
+    // return all restaurants via the `Restaurant.findAll()` method. 
+    const restaurants = await Restaurant.findAll();
+    // json() is a method that converts the data into JSON format [JavaScript Object Notation]
+    res.json(restaurants);
+})
 
 app.listen(port, () => {
     sequelize.sync();
