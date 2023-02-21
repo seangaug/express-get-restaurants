d6b12b6d1324119edbbcd9f15a62078fe520ba22
seangaug <seanjnr1991@aol.com>
2023-02-21 11:28:03 +0000

    part 2 route parameter added and test success

--- server.js
+++ server.js
@@ -5,13 +5,25 @@ const {sequelize} = require("./db");
 
 const port = 3000;
 
+//Part 1: Create a GET Request Route
+
 //TODO: Create your GET Request Route Below: 
-app.get("/restaurants", async (req, res) => {
-    // return all restaurants via the `Restaurant.findAll()` method. 
-    const restaurants = await Restaurant.findAll();
-    // json() is a method that converts the data into JSON format [JavaScript Object Notation]
-    res.json(restaurants);
+// app.get("/restaurants", async (req, res) => {
+//     // return all restaurants via the `Restaurant.findAll()` method. 
+//     const restaurants = await Restaurant.findAll();
+//     // json() is a method that converts the data into JSON format [JavaScript Object Notation]
+//     res.json(restaurants);
+// })
+
+//Part 2: Route Parameters
+
+app.get("/restaurants/:id", async (req, res) => {
+    // return a single restaurant via the `Restaurant.findByPk()` method. Find by 'Primary Key'
+    const restaurant = await Restaurant.findByPk(req.params.id);
+    // send found restaruant as a JSON response:
+    res.json(restaurant);
 })
+
 
 app.listen(port, () => {
     sequelize.sync();
